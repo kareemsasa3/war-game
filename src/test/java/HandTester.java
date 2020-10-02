3a402e4215c4bcc26c8b17ba32c2aed94900db8e
kareemsasa3 <kareemsasa3@gmail.com>
2020-10-01 22:56:50 -0500

    added tester classes

---
+++ src/test/java/HandTester.java
@@ -0,0 +1,17 @@
+import game.Hand;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HandTester {
+    private Hand hand;
+
+    @Before
+    public void setUp() throws Exception {
+        hand = new Hand();
+    }
+
+    @Test
+    public void testGive() throws Exception {
+
+    }
+}
